4b219aaa7b1fae893d0a5a9ace74ed2b5112c8b7
Sara Álvarez <saralvzlpz@gmail.com>
2023-02-07 13:51:24 +0100

    button finished but it needs routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import ButtonTrainer from './components/atoms/button-trainer/ButtonTrainer';
 
 function App() {
   return (
     <div className="App">
-      <ButtonTrainer />
+
     </div>
   );
 }
