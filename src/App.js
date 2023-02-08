beee75663199667045bec7e0418d345766e3a82a
adria15gomez <adria15gomez@gmail.com>
2023-02-08 20:41:43 +0100

    created play button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import PlayButton from "./components/atoms/atom-playButton/atom-PlayButton";
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Este es el boton de play</h1>
+      <PlayButton />
     </div>
   );
 }
