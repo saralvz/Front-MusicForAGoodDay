f90723fd455d082c95935d9b8e23a53d6498f6fa
adria15gomez <adria15gomez@gmail.com>
2023-02-09 12:18:02 +0100

    restore app.jsx

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import "./App.css";
 import SongBox from "./components/atoms/atom-songBox/atom-SongBox";
 
 function App() {
-  return (
-    <div className="App">
-      <SongBox />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
