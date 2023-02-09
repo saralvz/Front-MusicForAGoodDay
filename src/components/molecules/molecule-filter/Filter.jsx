43620ebc82651d71ef13ee32049cf9fe3357dceb
Ana <117080170+anavgg@users.noreply.github.com>
2023-02-09 10:26:41 +0100

    updating filter

--- src/components/molecules/molecule-filter/Filter.jsx
+++ src/components/molecules/molecule-filter/Filter.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
 import "../molecule-filter/filter.css";
+import filterNameArtist from "../../../assets/img/icons/filter-name-artist.svg";
+
 
 const Filter = () => {
-  const [checked, setChecked] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isPlayedChecked, setIsPlayedChecked] = useState(false);
+  const [isUnplayedChecked, setIsUnplayedChecked] = useState(false);
 
-  const handleChange = (event) => {
-    setChecked(event.target.checked);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handlePlayedCheck = () => {
+    setIsPlayedChecked(!isPlayedChecked);
+  };
+
+  const handleUnplayedCheck = () => {
+    setIsUnplayedChecked(!isUnplayedChecked);
   };
 
   return (
@@ -15,25 +27,29 @@ const Filter = () => {
         placeholder="Search by artist name"
         className="filter-input"
       />
-      <div className="played-checkbox-container">
-        <input
-          type="checkbox"
-          checked={checked}
-          onChange={handleChange}
-          className="checkbox"
-          id = "played"        />
-        <label className="checkbox-label">Played songs</label>
-      </div>
-      <div className="unplayed-checkbox-container">
-        <input
-          type="checkbox"
-          checked={checked}
-          onChange={handleChange}
-          className="checkbox"
-          id = "unplayed"
-        />
-        <label className="checkbox-label">Unplayed songs</label>
-      </div>
+      <img src={filterNameArtist} alt="Expand filter options" onClick={handleClick} />
+      {isOpen && (
+        <div className="filter-options">
+          <div className="played-checkbox-container">
+            <input
+              type="checkbox"
+              checked={isPlayedChecked}
+              onChange={handlePlayedCheck}
+              className="checkbox"
+            />
+            <label className="checkbox-label">Played songs</label>
+          </div>
+          <div className="unplayed-checkbox-container">
+            <input
+              type="checkbox"
+              checked={isUnplayedChecked}
+              onChange={handleUnplayedCheck}
+              className="checkbox"
+            />
+            <label className="checkbox-label">Unplayed songs</label>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
