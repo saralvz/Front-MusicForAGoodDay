360b06a79b4eb069111898938c431299d0da6417
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-02-09 18:03:45 +0100

    add function ShowSongBox

---
+++ src/services/ShowSongBox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SongBox from "../components/atoms/atom-songBox/atom-SongBox";
+
+function ShowSongBox({ inputValues }) {
+  return (
+    <SongBox 
+    image={register.img}
+    songName={register.songName}
+    artistName={register.artist}
+    songGender={register.gender}
+    coderName={register.yourName}/>
+  );
+}
+
+export default MostrarComponente;
