a843dbb5b9696f72c8d310316e37d4e81acce448
Jessica Ríos Maneiro <117263066+JessRm04@users.noreply.github.com>
2023-02-15 15:41:55 +0100

    solve bug

--- src/components/molecules/form/ValidationForm.jsx
+++ src/components/molecules/form/ValidationForm.jsx
@@ -50,19 +50,3 @@ const ValidationForm = () => {
 
 export default ValidationForm
 
-const btn = document.getElementById('btn');
-
-btn.addEventListener('click', () => {
-  const apiUrl = 'http://localhost:80/MusicForAGoodDay/songs/create';
-  
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      // haz algo con los datos recibidos de la API
-      console.log(data);
-    })
-    .catch(error => {
-      // maneja el error de la llamada a la API
-      console.error('Error fetching data:', error);
-    });
-});
